351d93b91f80dc89d856a1836ddec78db168a04b
marcial <marcial.maark@gmail.com>
2023-10-18 19:54:48 +0800

    Initial integration of user model

--- lucy/core/models.py
+++ lucy/core/models.py
@@ -1,3 +1,39 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    slug = models.CharField(
+        default=uuid.uuid4,
+        max_length=40,
+        editable=False,
+        db_index=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False, null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return str(self.slug)
+
+
+class User(BaseModel, AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
